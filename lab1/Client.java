9a0684d9ad3d1fe6e2af7be80818da08f6a3b67d
an-st-u <an-st.u@yandex.ru>
2015-10-13 10:42:52 +0500

    Update Client.java

--- lab1/Client.java
+++ lab1/Client.java
@@ -9,20 +9,21 @@ public class Client {
         String address = "127.0.0.1"; // IP-адрес компьютера, исполняется серверная программа.
         // Здесь указан адрес того самого компьютера где будет исполняться и клиент.
         try {
-            Socket socket = new Socket(address, serverPort);
+
+            Socket socket = new Socket(address, serverPort); // Cоздаём  сокету с портом 12345
 
             InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
             InputStreamReader  inputStreamReader= new InputStreamReader(inputStream);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
 
-            System.err.println(bufferedReader.readLine());
-
+            System.out.println(bufferedReader.readLine());
 
             Scanner cin = new Scanner(System.in);
-            OutputStream outputStream = socket.getOutputStream();
             outputStream.write((cin.nextLine()+"\n").getBytes());
 
-            System.err.println(bufferedReader.readLine());
+            System.out.println(bufferedReader.readLine());
             
 
 
@@ -31,5 +32,4 @@ public class Client {
         }
 
     }
-
 }
