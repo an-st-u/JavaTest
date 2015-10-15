189bcf38f6cf79b961de6cb75c3bfdee44fb6748
an-st-u <an-st.u@yandex.ru>
2015-10-15 12:38:59 +0500

    Update Server.java

--- lab1/Server.java
+++ lab1/Server.java
@@ -1,52 +1,37 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-public class Server {
-
+public class Server{
     private static ServerSocket sockets;
-
     public static void main(String[] args) {
         int serverPort = 12345; // порт
-
         try {
-            sockets = new ServerSocket(serverPort); /*Сокета  сервера ожидает
-                                                      ожидает запрос от клиента*/
+            sockets = new ServerSocket(serverPort); /*Сокета сервера ожидает
+ожидает запрос от клиента*/
         } catch (IOException e) {
             e.printStackTrace(); //Если порт занят
         }
-
-
         try {
             System.out.println("Сервер ожидает подключения");
             Socket socket = sockets.accept(); /*Экземпляр класса
-                                                Ожидает подключения к sockets*/
-
+Ожидает подключения к sockets*/
             System.out.println("Клиент установил подключение");
-
-            InputStream inputStream = socket.getInputStream();    //Входящий поток данных
+            InputStream inputStream = socket.getInputStream(); //Входящий поток данных
             OutputStream outputStream = socket.getOutputStream(); //Исходящий
-            //Возвращают поток байт
-
+//Возвращают поток байт
             outputStream.write(("Vvedite 2 chisla\n").getBytes());
-
             InputStreamReader inputStreamReader= new InputStreamReader(inputStream);
-            //Работа с входными потоком данных как со строками
+//Работа с входными потоком данных как со строками
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            //Буферизирует символы и позволяет извлекать как строки, так и символы
-
+//Буферизирует символы и позволяет извлекать как строки, так и символы
             String get=bufferedReader.readLine();
             System.out.println("Было получено: "+ get);
             NOD up = new NOD(get);
-
-            outputStream.write(("NOD = " + up.getResults() + "\n").getBytes());
+            outputStream.write(("НОД = "+up.getResult()+"\n").getBytes());
             outputStream.close();
-
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
