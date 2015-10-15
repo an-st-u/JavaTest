1a716548b57e9df6357af841af378887a5a5b135
an-st-u <an-st.u@yandex.ru>
2015-10-15 12:48:52 +0500

    Update HttpServer.java

--- lab2/HttpServer.java
+++ lab2/HttpServer.java
@@ -1,3 +1,17 @@
-public class HttpServer{
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer {
+
+    public static void main(String[] args) throws Throwable {
+        ServerSocket ss = new ServerSocket(8080);
+        while (true) {
+            Socket s = ss.accept();
+            System.err.println("Client accepted");
+            Thread T1 = new Thread(new SocketProcessor(s));
+            T1.start();
+        }
+    }
+
 
 }
