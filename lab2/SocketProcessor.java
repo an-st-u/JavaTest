6ec930b4e1f5492f9fba20aac05edf162c77dad8
an-st-u <an-st.u@yandex.ru>
2015-10-24 17:30:17 +0500

    Update SocketProcessor.java

--- lab2/SocketProcessor.java
+++ lab2/SocketProcessor.java
@@ -46,11 +46,11 @@ public void run() {
 
 
          String head = "<head><link rel=\"shortcut icon\" href=\"http://www.iconj.com/ico/h/9/h9arpg5dsi.ico\" type=\"image/x-icon\" /></head>\n";
-         String body = "<html>" + head + "<body><h1>" + "Вы ввели: " + get + "</h1></body></html>\n";
+         String body = "<html>" + head + "<body><h1>" + "Вы ввели: </h1><h2>" + get + "</h2></body></html>";
          String answer = "HTTP/1.1 200 OK\r\n" +
          "Server: Brig207\r\n" +
          "Content-Type: text/html; charset=UTF-8\r\n" +
-         "Content-Length: " + (body.length()) + "\r\n" +
+         "Content-Length: " + (body.getBytes().length) + "\r\n" +
          "Connection: close\r\n\r\n";
 
          System.out.println(answer + body);
@@ -92,21 +92,14 @@ private static String request(InputStreamReader inputStreamReader) throws IOExce
 
     private String index() {
 
-        String index = "<br>Работу выполняли:<br> Бергер Юлия<br> Сапронов Ярослав <br>Степакшин Андрей<br>" +
-                "Номер группы: РИ-330207 <br>" +
-                "Номер индивидуального задания: 3<br>" +
-                "Текст индивидуального задания:<br>" +
-                "НОД... <br>";
+        String index = "Работу выполняли:<br> Бергер Юлия<br> Сапронов Ярослав <br>Степакшин Андрей<br>\n" +
+                "Номер группы: РИ-330207 <br>\n" +
+                "Номер индивидуального задания: 10<br>\n" +
+                "Текст индивидуального задания:<br>\n" +
+                "Наибольший общий делитель(НОД) чисел<br>\n" +
+                "Числа должны поступать в виде строки<br> с некоторым разделителем.<br>\n";
 
-        String n="";
-        for (int i=0;i<index.length();i++)
-        {
-            n+="\r"; //Быдло код
-        }
-
-        return index+n;
+        return index;
     }
 
 }
-
-
