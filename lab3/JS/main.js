bca443a7aeb56a56239ec2fead2b076be340e160
an-st-u <an-st.u@yandex.ru>
2015-11-01 00:13:36 +0500

    Update main.js

--- lab3/JS/main.js
+++ lab3/JS/main.js
@@ -1,1 +1,15 @@
-words
+function sendText(){
+var elem = document.getElementById("nod_1").value;
+alert(elem);
+}
+
+function сlearDefault(){
+var elem = document.getElementById("nod_1").value;
+if (elem == "Вводить нужно здесь...") {
+document.getElementById("nod_1").value="";
+}
+}
+
+function сlear(){
+document.getElementById("nod_1").value="";
+}
