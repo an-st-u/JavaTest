3f5dcc917d1529f7caf516ad33c06bb8d43b0b86
an-st-u <an-st.u@yandex.ru>
2015-11-01 17:57:19 +0500

    Update main.js

--- lab3/JS/main.js
+++ lab3/JS/main.js
@@ -1,12 +1,21 @@
-function sendText(){
-var elem = document.getElementById("nod_1").value;
-alert(elem);
-}
-
 function сlearDefault(){
 var elem = document.getElementById("nod_1").value;
 if (elem == "Вводить нужно здесь...") {
 document.getElementById("nod_1").value="";
+}
+}
+
+function sendText(){
+var elem = document.getElementById("nod_1").value;
+var sendtext = new XMLHttpRequest();
+sendtext.open('POST','/JS/1.html',true);
+//sendtext.setRequestHeader('Content-Type', 'application/json');
+//sendtext.setRequestHeader('Content-Type', 'text/html');
+sendtext.send("elem="+elem);
+sendtext.onreadystatechange = function(e) {
+if(this.readyState ==4 && this.status == 200) {
+document.getElementById("nod_1").value = sendtext.responseText;
+}
 }
 }
 
