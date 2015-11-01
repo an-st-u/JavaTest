366e3b42c030924f757f43ed1077ee8622c6ea70
an-st-u <an-st.u@yandex.ru>
2015-11-01 21:51:45 +0500

    Update WebSite.java

--- lab3/WebSite.java
+++ lab3/WebSite.java
@@ -1,11 +1,19 @@
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class WebSite {
 
-    public static void main(String[] args) {
-        
-    System.out.println("Заглушка");
-    
+    public static void main(String[] args) throws Throwable{
+
+        ServerSocket ss = new ServerSocket(8080);
+
+        while (true) {
+            System.out.println("Сервер ожидает подключения");
+            Socket s = ss.accept();
+            System.out.println("Клиент установил соединение");
+            Thread T1 = new Thread(new SocketProcessor(s));
+            T1.start();
+        }
     }
 
 }
