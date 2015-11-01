c22636b71c5bb055f7811c73a57ce99c8ed5311b
an-st-u <an-st.u@yandex.ru>
2015-11-01 19:06:05 +0500

    Update main.js

--- lab3/JS/main.js
+++ lab3/JS/main.js
@@ -1,6 +1,6 @@
 function сlearDefault(){
 var elem = document.getElementById("nod_1").value;
-if (elem == "Вводить нужно здесь...") {
+if ((elem == "Вводить нужно здесь...") || (elem == "Введите в правильной форме, пожалуйста...") ) {
 document.getElementById("nod_1").value="";
 }
 }
@@ -9,8 +9,6 @@ function sendText(){
 var elem = document.getElementById("nod_1").value;
 var sendtext = new XMLHttpRequest();
 sendtext.open('POST','/JS/1.html',true);
-//sendtext.setRequestHeader('Content-Type', 'application/json');
-//sendtext.setRequestHeader('Content-Type', 'text/html');
 sendtext.send("elem="+elem);
 sendtext.onreadystatechange = function(e) {
 if(this.readyState ==4 && this.status == 200) {
