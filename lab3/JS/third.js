72bf72be4fdc6376a170f7feef0d87af3f024477
an-st-u <an-st.u@yandex.ru>
2015-11-03 14:03:04 +0500

    Update third.js

--- lab3/JS/third.js
+++ lab3/JS/third.js
@@ -1,20 +1,25 @@
 function add() {
 	
-	var suda = document.getElementById('tablitsa')
-	var kinoteatr = document.getElementById('nod_1').value;
+	var suda = document.getElementById('tablitsa');
+	var name = document.getElementById('name').value;
+	var address = document.getElementById('address').value;
+	var site = document.getElementById('site').value;
 	var tr = document.createElement('tr');
-	tr.id = 'dorem';
-	tr.innerHTML = '<td>'+kinoteatr+'</td><td>ул. Толмачева, 12</td><td><a href="http://www.kinosalut.ru/">Киносалют</a></td>';	
+	tr.innerHTML = '<td>'+name+'</td><td>'+address+'</td><td>'+site+'</td>';	
 	suda.appendChild(tr);
 }
 
 function _remove() {
-	var suda = document.getElementById('tablitsa')
-	var tr = document.getElementById('dorem')
-	suda.removeChild(tr);
+		
+	var suda = document.getElementById('tablitsa');
+	//alert(suda.lastElementChild.innerHTML);
+	if (suda.lastElementChild!=suda.firstElementChild) {
+	suda.removeChild(suda.lastElementChild);
+	}
 }
 
 function сlearDefault(){
+	
 	var elem = document.getElementById("nod_1").value;
 	if ((elem == "Вводить нужно здесь...") || (elem == "Введите в правильной форме, пожалуйста...") ) {
 	document.getElementById("nod_1").value="";
