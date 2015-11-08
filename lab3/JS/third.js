f5d7e8515bbdd0eae8f2888b090cc2753acb7eff
an-st-u <an-st.u@yandex.ru>
2015-11-08 21:53:52 +0500

    Update third.js

--- lab3/JS/third.js
+++ lab3/JS/third.js
@@ -1,33 +1,46 @@
 function addRow() {
 	
 	var suda = document.getElementById('tablitsa');
-	var name = document.getElementById('name').value;
-	var address = document.getElementById('address').value;
-	var site = document.getElementById('site').value;
 	
 	var row = suda.insertRow(-1);
-	addCell(row,name); // строка, тип, текст
-	addCell(row,address);
-	addCell(row,site);
-	addCell(row,"","input","submit");
-	
-	var delButton = row.childNodes[3].childNodes[0];
-	delButton.classList.add("del_btn");
-    	delButton.onclick = function() {
-		removeIndex(this);
+	addCellInput(row,"","text"); // строка, тип, текст
+	addCellInput(row,"","text");
+	addCellInput(row,"","text");
+	addCell(row,"Save","input","submit");
+
+
+	var Confirm = row.childNodes[3].childNodes[0];
+	Confirm.classList.add("del_btn");
+    	Confirm.onclick = function() {
+		confirm(this.parentNode.parentNode);
 	}
+
 }
 
-function addCell(row, in_text, tag, value_text) {
+function addCellInput(row, in_text, type_text,id) {
+
+	var cell = row.insertCell(-1);
+	var elem = document.createElement("input");
+	elem.classList.add("vvod");
+	elem.size = 25;
+	elem.type = type_text || null;
+	elem.value = in_text || "";
+	elem.innerHTML = in_text || "";
+	cell.appendChild(elem); 
+
+}
+
+
+function addCell(row, in_text, tag, type_text) {
 
 	var cell = row.insertCell(-1);
 	if (tag) {
 		var elem = document.createElement(tag);
-		elem.type = value_text || null;
+		elem.type = type_text || null;
 		elem.value = in_text || "";
 		elem.innerHTML = in_text || "";
-		cell.appendChild(elem); }
-	else {
+		cell.appendChild(elem); 
+	} else {
 		cell.innerHTML = in_text || "";
 	}
 	
@@ -55,4 +68,72 @@ function removeIndex(row) {
 	var suda = document.getElementById('tablitsa');
 	suda.deleteRow(row.parentNode.parentNode.rowIndex);
 
+}
+
+function confirm(row) {
+
+	var baza = [];
+	var temp = row.childNodes[3].childNodes[0];
+	row.removeChild(temp.parentNode);
+	
+	for (var i=2;i>=0;i--) {
+		
+		var Confirm = row.childNodes[i].childNodes[0];
+		baza[i]=Confirm.value;
+		row.removeChild(Confirm.parentNode);
+	}
+
+
+	for (var i=0;i<3;i++) {
+		addCell(row,baza[i]);	
+	}
+
+	addCell(row,"Del","input","submit");
+
+	var delButton = row.childNodes[3].childNodes[0];
+	delButton.classList.add("del_btn");
+    	delButton.onclick = function() {
+		removeIndex(this);
+	}
+
+	delButton = row.childNodes[3];
+	var trc = document.createElement('input');
+	delButton.appendChild(trc);
+	trc.value = "Edit";
+	trc.type = "submit";
+	trc.classList.add("del_btn");
+    	trc.onclick = function() {
+		EditRow(this.parentNode.parentNode,baza);
+	}
+	
+
+}
+
+function EditRow(row,baza) {
+
+		
+	for (var i=1;i>=0;i--) {
+		var Confirm = row.childNodes[3].childNodes[i];
+		Confirm.parentNode.removeChild(Confirm);
+	}
+
+	
+	for (var i=3;i>=0;i--) {
+		
+		var Confirm = row.childNodes[i];
+		row.removeChild(Confirm);
+	}
+
+	for (var i=2;i>=0;i--) {
+		addCellInput(row,baza[i],"text");
+	}
+
+	addCell(row,"Save","input","submit");
+
+
+	var Confirm = row.childNodes[3].childNodes[0];
+	Confirm.classList.add("del_btn");
+    	Confirm.onclick = function() {
+		confirm(this.parentNode.parentNode);
+	}
 }
