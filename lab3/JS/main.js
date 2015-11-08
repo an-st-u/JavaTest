05f2edf40b1a3decb4af87b4e7d36f78e3400197
an-st-u <an-st.u@yandex.ru>
2015-11-09 03:12:27 +0500

    Update main.js

--- lab3/JS/main.js
+++ lab3/JS/main.js
@@ -1,22 +1,26 @@
 function сlearDefault(){
-var elem = document.getElementById("nod_1").value;
-if ((elem == "Вводить нужно здесь...") || (elem == "Введите в правильной форме, пожалуйста...") ) {
-document.getElementById("nod_1").value="";
-}
+	var elem = document.getElementById("nod_1").value;
+	if ((elem == "Вводить нужно здесь...") || (elem == "Введите в правильной форме, пожалуйста...") ) {
+	document.getElementById("nod_1").value="";
+	}
 }
 
-function sendText(){
-var elem = document.getElementById("nod_1").value;
-var sendtext = new XMLHttpRequest();
-sendtext.open('POST','/JS/1.html',true);
-sendtext.send("elem="+elem);
-sendtext.onreadystatechange = function(e) {
-if(this.readyState ==4 && this.status == 200) {
-document.getElementById("nod_1").value = sendtext.responseText;
-}
-}
-}
+
 
 function сlear(){
-document.getElementById("nod_1").value="";
+	document.getElementById("nod_1").value="";
+}
+
+
+
+function sendText(){
+	var elem = document.getElementById("nod_1").value;
+	var sendtext = new XMLHttpRequest();
+	sendtext.open('POST','/sendNOD',true);
+	sendtext.send("elem="+elem);
+	sendtext.onreadystatechange = function(e) {
+	if(this.readyState ==4 && this.status == 200) {
+		document.getElementById("nod_1").value = sendtext.responseText;
+		}
+	}
 }
